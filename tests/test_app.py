46e23ce7d2f73591cd0bd21202c906d8d3a66bc0
Shalu Kumari <shalu.kumari@abzooba.com>
2024-02-13 20:04:10 +0530

    AppChanged

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
 from app import index
 
 def test_index():
-    assert index()=="Hello,World, this is pipetesting demo!"
+    assert index()=="Hello,World, this is pipetesting demo! New Changes made"
